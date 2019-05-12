acf43a499e1732534f08b8c5b8bedaf06288288f
Michael Huxtable <mig_uk@hotmail.com>
2019-05-12 16:40:57 +0100

    Add tests for index vue

---
+++ test/index.vue.spec.js
@@ -0,0 +1,58 @@
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import Index from '~/pages/bugs/index.vue'
+
+const localVue = createLocalVue()
+localVue.config.productionTip = false
+localVue.use(Vuex)
+localVue.use(Vuetify)
+
+describe('bugs/index', () => {
+  let wrapper = null
+
+  beforeEach(() => {
+    wrapper = mount(Index, {
+      computed: {
+        bugs: () => [
+          {
+            id: 1,
+            title: 'my bug',
+            description: 'test',
+            dateCreated: new Date(2019, 0, 1)
+          },
+          {
+            id: 2,
+            title: 'not my bug',
+            description: 'test #2',
+            dateCreated: new Date(2019, 1, 1)
+          }
+        ]
+      },
+      stubs: { NuxtLink: RouterLinkStub },
+      localVue
+    })
+  })
+
+  it('renders the bug titles', () => {
+    const titles = wrapper
+      .findAll('.v-list__tile__title')
+      .wrappers.map(i => i.text().trim())
+
+    const expected = ['my bug', 'not my bug']
+    expect(titles).toEqual(expected)
+  })
+
+  it('renders the bug dates', () => {
+    const dates = wrapper
+      .findAll('.v-list__tile__sub-title')
+      .wrappers.map(i => i.text().trim())
+
+    // TODO: Should be a more sensible date format
+    const expected = [
+      'Tue Jan 01 2019 00:00:00 GMT+0000 (Greenwich Mean Time)',
+      'Fri Feb 01 2019 00:00:00 GMT+0000 (Greenwich Mean Time)'
+    ]
+    expect(dates).toEqual(expected)
+  })
+})
