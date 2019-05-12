d8a6a92e60b3328ad25d9c8be168e87675aa6807
Michael Huxtable <mig_uk@hotmail.com>
2019-05-12 17:25:25 +0100

    Remove new bug vue file and add to page instead of component

--- pages/bugs/new.vue
+++ pages/bugs/new.vue
@@ -1,17 +1,61 @@
 <template>
   <v-layout align-center>
     <v-flex xs12>
-      <new-bug />
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="title"
+          :counter="100"
+          :rules="titleRules"
+          label="Title"
+          required
+        ></v-text-field>
+        <v-textarea
+          v-model="description"
+          :rules="descriptionRules"
+          label="Description"
+          required
+        ></v-textarea>
+
+        <v-btn :disabled="!valid" color="success" @click="submit">Submit</v-btn>
+        <v-btn color="error" @click="reset">Reset Form</v-btn>
+      </v-form>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import NewBug from '~/components/bugs/NewBug.vue'
-
 export default {
-  components: {
-    NewBug
+  components: {},
+  data: () => ({
+    valid: true,
+    title: '',
+    titleRules: [
+      v => !!v || 'Title is required',
+      v => (v && v.length <= 100) || 'Title must be less than 100 characters'
+    ],
+    description: '',
+    descriptionRules: [v => !!v || 'Description is required']
+  }),
+  methods: {
+    submit() {
+      if (!this.$refs.form.validate()) {
+        return
+      }
+
+      const todo = {
+        title: this.title,
+        description: this.description
+      }
+
+      this.$store.commit('bugs/add', todo)
+      this.$router.push('/bugs')
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    }
   }
 }
 </script>
