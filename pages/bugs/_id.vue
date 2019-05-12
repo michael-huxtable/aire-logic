078fd504adc6d190071aa582b48fecd951bf0b3f
Michael Huxtable <mig_uk@hotmail.com>
2019-05-12 10:20:37 +0100

    add back button to view page and validate id

--- pages/bugs/_id.vue
+++ pages/bugs/_id.vue
@@ -9,6 +9,10 @@
 
         <h3>Date Created</h3>
         <p>{{ bug.dateCreated }}</p>
+
+        <v-btn color="information" :to="'/bugs'">
+          Go Back
+        </v-btn>
       </v-form>
     </v-flex>
   </v-layout>
@@ -17,6 +21,9 @@
 <script>
 export default {
   components: {},
+  validate({ params }) {
+    return !isNaN(+params.id)
+  },
   data() {
     const params = this.$route.params
     const bug = this.$store.getters['bugs/getById'](+params.id)
